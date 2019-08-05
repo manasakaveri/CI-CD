94b1834e01eb6c9f026a33c55c8502ef68a999c2
Manasa Kaveri <manasakaveri@Manasas-Air.attlocal.net>
2019-08-05 09:55:06 -0700

    First commit

---
+++ PycharmProjects/CI/E1.py
@@ -0,0 +1,17 @@
+my_name = "Manasa kaveri"
+my_age = 35 #its  real
+my_height= 156 #inches
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair ='Black'
+
+print(f"lets talk about  { my_name}. ")
+print(f"Her {my_height} inches tall .")
+print(f" Her{my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her  teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}")
