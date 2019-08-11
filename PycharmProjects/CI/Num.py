c168d2a650b47ccd63c056f8b3be8b29bad1e3e1
Manasa Kaveri <manasakaveri@Manasas-MacBook-Air.local>
2019-08-10 23:21:00 -0700

    Num.py

---
+++ PycharmProjects/CI/Num.py
@@ -0,0 +1,37 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Number1",help ="First number")
+    parser.add_argument("Number2", help="second number")
+    parser.add_argument("operation",help="operation, operation i support is add ,multipilcation ,division ,subtraction")
+
+
+args = parser.parse_args()
+print("Argument 1 is "+ args.Number1)
+print("Argument 2 is "+args.Number2)
+
+
+if args.operation == "add":
+    result = int(args.Number1) + int(args.Number2)
+    abc = "string 1 " + "string 2"
+    print("Combined string is  :" + abc)
+    print("operation is addition and the value is  : " + str(result))
+
+
+elif args.operation == 'multiplication':
+     result = int(args.Number1) * int(args.Number2)
+
+     print("operation is multiplication and the value is : " + str(result))
+
+elif args.operation == "division":
+    result = int(args.Number1) / int(args.Number2)
+    print("operation is division  and the value is : " + str(result))
+
+elif args.operation == "subtraction":
+    result = int(args.Number1) - int(args.Number2)
+    print("operation is subtraction   and the value is : " + str(result))
+
+else:
+    print("i dont understand operation you have specified. Operation you have specified " +args.operation +" is unknown.")
+
